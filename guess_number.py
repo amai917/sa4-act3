17b92f9804ebc74a0ad04ec0904a6f1c6fcec35b
Ali Mai <alimai3213@gmail.com>
2024-02-07 17:53:17 -0600

    Have the user asked to guess till they enter q to quit

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,16 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+while True:
+    guess = input("What number am I thinking of? ")
+
+    if guess == 'q':
+        print(f"Sorry! The number was {number}.")
+        break
+
+    if guess.isdigit() and int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break 
+    else:
+        print("Sorry! please try again or enter q to quit.")
