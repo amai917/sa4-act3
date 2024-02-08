a22d91482d7b5793ac8859240e5fb7ccd3337e21
Ali Mai <alimai3213@gmail.com>
2024-02-07 20:18:45 -0600

    Added limited guesses

--- guess_number.py
+++ guess_number.py
@@ -1,16 +1,28 @@
 number = 10
+guesses_left = 5
 
-print("I'm thinking of a number...")
+print("I'm thinking of a number... You have 5 guesses.")
 
-while True:
+while guesses_left > 0:
     guess = input("What number am I thinking of? ")
 
     if guess == 'q':
         print(f"Sorry! The number was {number}.")
         break
 
-    if guess.isdigit() and int(guess) == number:
-        print("Congratulations! You guessed the right number.")
-        break 
+    if guess.isdigit():
+        guess = int(guess)
+        if guess == number:
+            print("Congratulations! You guessed the right number.")
+            break
+        elif guess < number:
+            print(f"Too low. You have {guesses_left - 1} guesses left.")
+        else:
+            print(f"Too high. You have {guesses_left - 1} guesses left.")
     else:
-        print("Sorry! please try again or enter q to quit.")
+        print(f"Please try again. You have {guesses_left - 1} guesses left.")
+
+    guesses_left -= 1
+
+if guesses_left == 0:
+    print(f"Sorry, you've run out of guesses. The number was {number}.")
