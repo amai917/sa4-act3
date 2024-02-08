85de4dc0a3338c3079f1f1dfb488a876e4bd4303
Ali Mai <alimai3213@gmail.com>
2024-02-07 20:08:21 -0600

    Added hints to help

--- guess_number.py
+++ guess_number.py
@@ -3,14 +3,18 @@ number = 10
 print("I'm thinking of a number...")
 
 while True:
-    guess = input("What number am I thinking of? ")
+    guess = input("What number am I thinking of? ('q' to quit) ")
 
     if guess == 'q':
         print(f"Sorry! The number was {number}.")
         break
 
-    if guess.isdigit() and int(guess) == number:
-        print("Congratulations! You guessed the right number.")
-        break 
-    else:
-        print("Sorry! please try again or enter q to quit.")
+    if guess.isdigit():
+        guess = int(guess)
+        if guess == number:
+            print("Congratulations! You guessed the right number.")
+            break
+        elif guess < number:
+            print("Too low")
+        else:
+            print("Too high")
